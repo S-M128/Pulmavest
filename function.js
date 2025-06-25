6a0c8a49e27a6e02a02c0b1041bfac57b76f87d9
S-M128 <siddique.m0102008@gmail.com>
2025-06-25 20:35:55 +0100

    new changes

--- function.js
+++ function.js
@@ -7,20 +7,6 @@
 //    }, 2000); 
 //  });
 
-// Dropdown Nav menu 
-
-    let menuList = document.getElementById("menuList");
-    menuList.style.maxHeight = "0px";
-
-    function toggleMenu() {
-        if (menuList.style.maxHeight === "0px") {
-            menuList.style.maxHeight = "300px";
-        } else {
-            menuList.style.maxHeight = "0px";
-        }
-    }
-
-
 let paragraphs = [...document.querySelectorAll('.reveal-type')];
 let spans = [];
 
